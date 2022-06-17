cd27b03ca6ff6a1b439ce64df211f8cc444a22c1
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-06-17 18:55:51 +0200

    changed url

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-const BASE_URL = 'http://localhost:8080';
+//const BASE_URL = 'http://localhost:8080';
 
-//const BASE_URL = 'https://final-project-garden-planner.herokuapp.com';
+const BASE_URL = 'https://final-project-garden-planner.herokuapp.com';
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
 
