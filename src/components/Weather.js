f56b1f2cbbc4ff3b78730b6300b6878384dfe296
SofiePellegrini <sofie.pellegrini@outlook.com>
2022-06-20 23:44:19 +0200

    apikey

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import moment from 'moment';
 
-import { API_KEY } from 'utils/api';
+import { API_KEY } from 'api/api';
 
 import {
   WeatherWrapper,
